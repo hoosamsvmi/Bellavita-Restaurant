a1efc49ea9be32d106f5d8148418ff92d44dea8a
Hossam Samy <hossamsamyabdelaal@gmail.com>
2026-07-16 01:24:33 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,36 +1,36 @@
-if ('scrollRestoration' in history) { 
-    history.scrollRestoration = 'manual';
-};
-window.addEventListener("load", function(){
-    window.scrollTo(0,0);
+if ("scrollRestoration" in history) {
+  history.scrollRestoration = "manual";
+}
+window.addEventListener("load", function () {
+  window.scrollTo(0, 0);
 });
 
 const navbuttons = document.querySelectorAll("[data-section]");
-navbuttons.forEach(btn => {
-        btn.addEventListener("click", function () {
-        const sectionid = btn.getAttribute("data-section");
-        const section = document.getElementById(sectionid);
-        section.scrollIntoView({
-            behavior: "smooth"
-        });
+navbuttons.forEach((btn) => {
+  btn.addEventListener("click", function () {
+    const sectionid = btn.getAttribute("data-section");
+    const section = document.getElementById(sectionid);
+    section.scrollIntoView({
+      behavior: "smooth",
     });
+  });
 });
 const herobutton = document.getElementById("view-menu-btn");
 const menu = document.getElementById("menu");
-herobutton.addEventListener("click" , function (){
-    menu.scrollIntoView();
+herobutton.addEventListener("click", function () {
+  menu.scrollIntoView();
 });
 
 const sections = document.querySelectorAll("section");
-window.addEventListener("scroll" , function(){
-    sections.forEach(section => {
+window.addEventListener("scroll", function () {
+  sections.forEach((section) => {
     const windowHeight = window.innerHeight;
     const sectionTop = section.getBoundingClientRect().top;
     const revealPoint = 100;
     if (sectionTop < windowHeight - revealPoint) {
-        section.classList.add("show")
-    } else{
-        section.classList.remove("show")
-    };
-    });
+      section.classList.add("show");
+    } else {
+      section.classList.remove("show");
+    }
+  });
 });
